6771d871d0c4943fbc0829cb2006f4b462138fd7
Leonardo Sanner <leosanner12@gmail.com>
2026-09-10 14:08:34 -0300

    .

--- api/src/main/java/com/telai/api/kernel/error/Handler.java
+++ api/src/main/java/com/telai/api/kernel/error/Handler.java
@@ -2,19 +2,41 @@ package com.telai.api.kernel.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class Handler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
+        var errors = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                FieldError::getField,
+                                Collectors.mapping(
+                                        err -> Map.of(
+                                                "message", err.getDefaultMessage(),
+                                                "rejectedValue", err.getRejectedValue()
+                                        ),
+                                        Collectors.toList()
+                                )
+                        )
+                );
+
+
         return new ResponseEntity<>(
-                ErrorResponse.fromException(ex),
+                errors,
                 HttpStatus.BAD_REQUEST
         );
     }
